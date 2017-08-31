4eed02d895b2e8a3306f6ddc7274231cce4d28c0
MOluwole <oluwole564@gmail.com>
2017-08-31 08:43:39 +0100

    Better Video to play offline videos

--- app/src/main/java/com/example/yung/streamer/OfflinePlay.kt
+++ app/src/main/java/com/example/yung/streamer/OfflinePlay.kt
@@ -1,14 +1,10 @@
 package com.example.yung.streamer
 
-import android.media.MediaCodec
 import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.SurfaceView
-import com.google.android.exoplayer.*
-import com.google.android.exoplayer.extractor.ExtractorSampleSource
-import com.google.android.exoplayer.upstream.DefaultAllocator
-import com.google.android.exoplayer.upstream.DefaultUriDataSource
+import com.google.android.exoplayer.ExoPlayer
+import com.halilibo.bettervideoplayer.BetterVideoPlayer
 
 
 class OfflinePlay : AppCompatActivity() {
@@ -22,26 +18,32 @@ class OfflinePlay : AppCompatActivity() {
         setContentView(R.layout.activity_offline_play)
 
         val video_uri: Uri = Uri.parse(intent.getStringExtra("File"))
-        val surfaceView = findViewById(R.id.surface_view) as SurfaceView
 
-        val allocator = DefaultAllocator(BUFFER_SEGMENT_SIZE)
-        val dataSource = DefaultUriDataSource(this, "userAgent")
-        // val S = ExtractorSampleSource()
-        val sampleSource = ExtractorSampleSource(video_uri, dataSource, allocator, BUFFER_SEGMENT_COUNT * BUFFER_SEGMENT_SIZE)
-        val videoRenderer = MediaCodecVideoTrackRenderer(
-                this, sampleSource, MediaCodecSelector.DEFAULT, MediaCodec.VIDEO_SCALING_MODE_SCALE_TO_FIT)
-        val audioRenderer = MediaCodecAudioTrackRenderer(
-                sampleSource, MediaCodecSelector.DEFAULT)
-        val rendererArray = arrayOf<TrackRenderer>(videoRenderer, audioRenderer)
-
-        exoPlayer = ExoPlayer.Factory.newInstance(rendererArray.size)
-        exoPlayer?.prepare(*rendererArray)
-        exoPlayer?.sendMessage(videoRenderer, MediaCodecVideoTrackRenderer.MSG_SET_SURFACE, surfaceView.holder.surface)
-        exoPlayer?.playWhenReady = true
+        val video_player = findViewById(R.id.player) as BetterVideoPlayer
+        video_player.enableControls()
+        video_player.enableSwipeGestures()
+        video_player.setSource(video_uri)
+        video_player.start()
+//        val surfaceView = findViewById(R.id.surface_view) as SurfaceView
+//
+//        val allocator = DefaultAllocator(BUFFER_SEGMENT_SIZE)
+//        val dataSource = DefaultUriDataSource(this, "userAgent")
+//        // val S = ExtractorSampleSource()
+//        val sampleSource = ExtractorSampleSource(video_uri, dataSource, allocator, BUFFER_SEGMENT_COUNT * BUFFER_SEGMENT_SIZE)
+//        val videoRenderer = MediaCodecVideoTrackRenderer(
+//                this, sampleSource, MediaCodecSelector.DEFAULT, MediaCodec.VIDEO_SCALING_MODE_SCALE_TO_FIT)
+//        val audioRenderer = MediaCodecAudioTrackRenderer(
+//                sampleSource, MediaCodecSelector.DEFAULT)
+//        val rendererArray = arrayOf<TrackRenderer>(videoRenderer, audioRenderer)
+//
+//        exoPlayer = ExoPlayer.Factory.newInstance(rendererArray.size)
+//        exoPlayer?.prepare(*rendererArray)
+//        exoPlayer?.sendMessage(videoRenderer, MediaCodecVideoTrackRenderer.MSG_SET_SURFACE, surfaceView.holder.surface)
+//        exoPlayer?.playWhenReady = true
     }
 
     override fun onBackPressed() {
-        exoPlayer?.stop()
+//        exoPlayer?.stop()
         finish()
     }
 }
